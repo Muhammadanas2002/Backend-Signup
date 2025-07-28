471ffe3dc5e0994df953c5a1f6b617bcf3800c9f
Aaliyan Khan <aaliyank552@gmail.com>
2025-07-28 21:50:34 +0500

    changes

--- app.js
+++ app.js
@@ -9,64 +9,67 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 
-const port = process.env.port || 5000   
+const port = process.env.port || 5000
 
 
 const URI = process.env.MONGO;
 
 mongoose.connect(URI)
-.then(()=>{
-    console.log("dbconnected");
-    
-}).catch((error)=>{
-    console.log("not connected" ,error);
-})
+    .then(() => {
+        console.log("dbconnected");
 
-app.get("/",(req,res )=>{
-        res.send("server is up")
-    }
-    )
-
-
-
-    app.post("/signup", async(req,res)=>{
-            const body = req.body
-            
-
-                const isExist = await UserModel.findOne({Email: body.Email})
-
-                    if(isExist){
-                          return  res.json({
-                                status: false,
-                                message:"Email already Exist",
-                                default:null
-                            })
-
-                
-            }
-
-
-                const hashPass = await bcrypt.hash(body.password,10);
-                const userObj= {
-                    ...body,
-                        password: hashPass
-                }
-
-
-            await UserModel.create(userObj)
-            console.log("user signup");
-
-            res.json({
-                message :"user created successfully!!"
-            })
-
-
-
-           
-            
-
-            
+    }).catch((error) => {
+        console.log("not connected", error);
     })
+
+app.get("/", (req, res) => {
+    res.send("server is up")
+}
+)
+
+
+
+app.post("/signup", async (req, res) => {
+
+    res.send("signup api ")
+
+    const body = req.body
+
+
+    const isExist = await UserModel.findOne({ Email: body.Email })
+
+    if (isExist) {
+        return res.json({
+            status: false,
+            message: "Email already Exist",
+            default: null
+        })
+
+
+    }
+
+
+    const hashPass = await bcrypt.hash(body.password, 10);
+    const userObj = {
+        ...body,
+        password: hashPass
+    }
+
+
+    await UserModel.create(userObj)
+    console.log("user signup");
+
+    res.json({
+        message: "user created successfully!!"
+    })
+
+
+
+
+
+
+
+})
 
 // app.get()
 
@@ -82,7 +85,7 @@ app.get("/",(req,res )=>{
 
 
 
-app.listen(port,()=>console.log(`runnig on ${port}`)
+app.listen(port, () => console.log(`runnig on ${port}`)
 );
 
 
